67c89195dfa0fba3ee9d140a0219995b95c93294
Peter Vrba <phonkee@phonkee.eu>
2016-11-22 13:00:14 +0100

    Regenerated templates

--- templates/gen_templates.sh
+++ templates/gen_templates.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-go-bindata-assetfs -pkg templates -nocompress -prefix templates -ignore .+\.go$ -debug .
+go-bindata-assetfs -pkg templates -nocompress -prefix templates -ignore .+\.go$ -ignore .+\.sh$ .
 mv bindata_assetfs.go templates.go
 
