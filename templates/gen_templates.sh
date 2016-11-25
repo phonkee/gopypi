829e17e0a309dfa5fb6cc198a385fc16c287fbdb
Peter Vrba <phonkee@users.noreply.github.com>
2016-11-25 06:14:11 +0100

    Update gen_templates.sh

--- templates/gen_templates.sh
+++ templates/gen_templates.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-go-bindata-assetfs -pkg templates -nocompress -prefix templates -ignore .+\.go$ -ignore .+\.sh$ .
+go-bindata-assetfs -pkg templates -prefix templates -ignore .+\.go$ -ignore .+\.sh$ .
 mv bindata_assetfs.go templates.go
 
